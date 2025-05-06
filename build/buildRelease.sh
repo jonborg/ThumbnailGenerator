0ee86892ec8e978cc70fd2200cc7b8cda0ee6d42
Joao Borges <joaomanborges@hotmail.com>
2025-05-07 00:05:56 +0100

    Program is run with a custom JRE Created script that creates custom JRE

--- build/buildRelease.sh
+++ build/buildRelease.sh
@@ -8,10 +8,21 @@ ASSET_FOLDER="assets/tournaments"
 ASSET_MASK_FOLDER="assets/masks"
 ASSET_DOCUMENTATION_FOLDER="assets/documentation"
 SETTING_FOLDER="settings"
+JRE_FOLDER="jre"  
 
-echo "Creating release $VERSION of Thumbnail Generator";
-mvn clean install
+echo "Creating release $VERSION of Thumbnail Generator"
+mvn clean install -Dmaven.test.skip=true
 
 echo "Moving $EXE_FILE to zip"
-tar -a -c -f "${THUMB// /}_v${VERSION//./_}.zip" "$EXE_FILE" "$README_FILE" "$THUMB_EXAMPLE_FILE" "$TOP_EXAMPLE_FILE" "$ASSET_FOLDER" "$ASSET_MASK_FOLDER" "$ASSET_DOCUMENTATION_FOLDER" "$SETTING_FOLDER"
+tar -a -c -f "${THUMB// /}_v${VERSION//./_}.zip" \
+  "$EXE_FILE" \
+  "$README_FILE" \
+  "$THUMB_EXAMPLE_FILE" \
+  "$TOP_EXAMPLE_FILE" \
+  "$ASSET_FOLDER" \
+  "$ASSET_MASK_FOLDER" \
+  "$ASSET_DOCUMENTATION_FOLDER" \
+  "$SETTING_FOLDER" \
+  "$JRE_FOLDER"
+
 echo "Successfully created zip file ${THUMB// /}_v${VERSION//./_}.zip"
