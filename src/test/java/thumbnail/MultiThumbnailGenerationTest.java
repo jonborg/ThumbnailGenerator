7a76a8ff7fbea9187ee2585daa8e46312bd5123d
Joao Borges <joaomanborges@hotmail.com>
2025-05-27 00:21:58 +0100

    Add unit test for multi thumbnail generation, where two characters are provided per player

--- src/test/java/thumbnail/MultiThumbnailGenerationTest.java
+++ src/test/java/thumbnail/MultiThumbnailGenerationTest.java
@@ -19,6 +19,7 @@ import utils.WaitUtils;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
@@ -68,10 +69,12 @@ public class MultiThumbnailGenerationTest {
         InputStream fileInputsStream = new FileInputStream(thumbnailListFile);
         thumbnailService.generateAndSaveThumbnailsFromFile(fileInputsStream, false, loadingState);
 
-        WaitUtils.waitInSeconds(40);
         //Assert
         for (int i=0; i<3; i++) {
             var thumbnailExists = WaitUtils.waitForFile(actualThumbnails.get(i));
+            if(i==0){
+                WaitUtils.waitInSeconds(2);
+            }
             assertTrue(thumbnailExists);
             assertArrayEquals(
                     Files.readAllBytes(expectedThumbnails.get(i).toPath()),
@@ -107,9 +110,11 @@ public class MultiThumbnailGenerationTest {
         thumbnailService.generateAndSaveThumbnailsFromFile(fileInputsStream, false, loadingState);
 
         //Assert
-        WaitUtils.waitInSeconds(40);
         for (int i=0; i<3; i++) {
             var thumbnailExists = WaitUtils.waitForFile(actualThumbnails.get(i));
+            if(i==0){
+                WaitUtils.waitInSeconds(2);
+            }
             assertTrue(thumbnailExists);
             assertArrayEquals(
                     Files.readAllBytes(expectedThumbnails.get(i).toPath()),
@@ -152,10 +157,12 @@ public class MultiThumbnailGenerationTest {
         InputStream fileInputsStream = new FileInputStream(thumbnailListFile);
         thumbnailService.generateAndSaveThumbnailsFromFile(fileInputsStream, true, loadingState);
 
-        WaitUtils.waitInSeconds(15);
         //Assert
         for (int i=0; i<3; i++) {
             var thumbnailExists = WaitUtils.waitForFile(actualThumbnails.get(i));
+            if(i==0){
+                WaitUtils.waitInSeconds(2);
+            }
             assertTrue(thumbnailExists);
             assertArrayEquals(
                     Files.readAllBytes(expectedThumbnails.get(i).toPath()),
@@ -166,6 +173,42 @@ public class MultiThumbnailGenerationTest {
             assertTrue(characterImages.get(i*2 + 1).exists());
             assertTrue(characterImages.get(i*2).delete());
             assertTrue(characterImages.get(i*2 + 1).delete());
+        }
+    }
+
+    @Test
+    public void create_validThumbnailFileDoubleCharacterPerPlayer_success()
+            throws IOException,
+            FighterImageSettingsNotFoundException, InterruptedException {
+        var loadingState = new LoadingState(false,true ,0 ,0);
+        File thumbnailListFile = new File(getClass().getResource("/input/multiThumbnailDoubleCharacterGeneration.txt").getPath());
+        List<File> expectedThumbnails = Arrays.asList(
+                FileUtils.getFileFromResources(
+                        "/expected/thumbnail/multiThumbnailDoubleCharacter1.png"),
+                FileUtils.getFileFromResources(
+                        "/expected/thumbnail/multiThumbnailDoubleCharacter2.png")
+        );
+        List<File> actualThumbnails = Arrays.asList(
+                FileUtils.getActualFile("/generated_thumbnails/UrQte-robin11--Arjay-ganondorf5--Losers-20_02_20XX.png"),
+                FileUtils.getActualFile("/generated_thumbnails/Aegis-corrin28--Toaster-marth5--winners Semis-20_02_20XX.png")
+        );
+
+        //Act
+        InputStream fileInputsStream = new FileInputStream(thumbnailListFile);
+        thumbnailService.generateAndSaveThumbnailsFromFile(fileInputsStream, false, loadingState);
+
+        //Assert
+        for (int i=0; i<2; i++) {
+            var thumbnailExists = WaitUtils.waitForFile(actualThumbnails.get(i));
+            if(i==0){
+                WaitUtils.waitInSeconds(2);
+            }
+            assertTrue(thumbnailExists);
+            assertArrayEquals(
+                    Files.readAllBytes(expectedThumbnails.get(i).toPath()),
+                    Files.readAllBytes(actualThumbnails.get(i).toPath())
+            );
+            assertTrue(actualThumbnails.get(i).delete());
         }
     }
 }
