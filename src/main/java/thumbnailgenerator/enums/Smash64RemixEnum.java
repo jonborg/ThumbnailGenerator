267904dfd58a1793adc15db9e4041ac3087ae2cc
Joao Borges <joaomanborges@hotmail.com>
2025-05-27 17:39:28 +0100

    Added SSB64Remix remix characters Resized SSB64Remix stock icons

--- src/main/java/thumbnailgenerator/enums/Smash64RemixEnum.java
+++ src/main/java/thumbnailgenerator/enums/Smash64RemixEnum.java
@@ -18,7 +18,27 @@ public enum Smash64RemixEnum implements CharacterEnum {
     LUIGI("Luigi", "luigi", 93, 7),
     NESS("Ness", "ness", 95, 7),
     CAPTAIN_FALCON("Captain Falcon", "captain_falcon", 94, 8),
-    JIGGLYPUFF("Jigglypuff", "jigglypuff", 96, 8);
+    JIGGLYPUFF("Jigglypuff", "jigglypuff", 96, 8),
+
+    //REMIX - no start.gg
+    GANONDORF("Ganondorf", "ganondorf", -1, 7),
+    YOUNG_LINK("Young Link", "young_link", -1, 7),
+    FALCO("Falco", "falco", -1, 6),
+    DR_MARIO("Dr. Mario", "dr_mario", -1, 6),
+    DARK_SAMUS("Dark Samus", "dark_samus", -1, 7),
+    WARIO("Wario", "wario", -1, 6),
+    LUCAS("Lucas", "lucas", -1, 6),
+    BOWSER("Bowser", "bowser", -1, 6),
+    WOLF("Wolf", "wolf", -1, 7),
+    CONKER("Conker", "conker", -1, 7),
+    MEWTWO("Mewtwo", "mewtwo", -1, 6),
+    MARTH("Marth", "marth", -1, 6),
+    SONIC("Sonic", "sonic", -1, 12),
+    SHEIK("Sheik", "sheik", -1, 7),
+    MARINA("Marina", "marina", -1, 6),
+    KING_DEDEDE("King Dedede", "king_dedede", -1, 6),
+    GOEMON("Goemon", "goemon", -1, 7),
+    BANJO_AND_KAZOOIE("Banjo & Kazooie", "banjo_and_kazooie", -1, 7);
 
     private final String name;
     private final String code;
